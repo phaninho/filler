60a2465c4359a3630306bb52b16b3e176590f934
Stephane MARTINS <stmartin@e2r9p10.42.fr>
2016-11-29 17:11:23 +0100

    add fctn to find same sign

--- include/filler.h
+++ include/filler.h
@@ -6,7 +6,7 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 17:00:45 by stmartin          #+#    #+#             */
-/*   Updated: 2016/11/25 16:40:19 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/11/29 15:44:50 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ typedef struct		s_env
 	char	**board;
 	char	*buff;
 	char	**piece;
+	int		sbx;
+	int		sby;
+	int		px;
+	int		py;
+	int		spx;
+	int		spy;
 }					t_env;
 
 #endif
--- src/filler.c
+++ src/filler.c
@@ -6,40 +6,35 @@
 /*   By: stmartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 16:59:57 by stmartin          #+#    #+#             */
-/*   Updated: 2016/11/25 17:55:55 by stmartin         ###   ########.fr       */
+/*   Updated: 2016/11/29 17:10:48 by stmartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "filler.h"
 
-void		board_alloc(t_env *e)
+void		board_alloc(t_env *e, int i)
 {
-	int		x;
-	int		y;
-	int		i;
-
-	i = -1;
 	if (e->buff[9] == '5')
 	{
-		x = 17;
-		y = 15;
+		e->sbx = 17;
+		e->sby = 15;
 	}
 	if (e->buff[9] == '4')
 	{
-		x = 40;
-		y = 24;
+		e->sbx = 40;
+		e->sby = 24;
 	}
 	if (e->buff[9] == '0')
 	{
-		x = 99;
-		y = 100;
+		e->sbx = 99;
+		e->sby = 100;
 	}
-	e->board = (char **)malloc(sizeof(char *) * (y + 1));
-	e->board[y] = NULL;
-	while (++i < y)
+	e->board = (char **)malloc(sizeof(char *) * (e->sby + 1));
+	e->board[e->sby] = NULL;
+	while (++i < e->sby)
 	{
-		e->board[i] = (char *)malloc(sizeof(char ) * (x + 1));
-		e->board[i][x] = '\0';
+		e->board[i] = (char *)malloc(sizeof(char) * (e->sbx + 1));
+		e->board[i][e->sbx] = '\0';
 	}
 }
 
@@ -63,19 +58,53 @@ void		fill_board(t_env *e)
 	}
 	free(lnb);
 }
+#include <stdio.h>
+void		play_mf(t_env *e, char c)
+{
+	int		x;
+	int		y;
 
-void		fill_piece(t_env *e, int x, int y)
+	y = 0;
+	while (e->board[y])
+	{
+		x = 0;
+	//	fprintf(stderr, "entre x:%d y:%d [", x, y);
+	//	ft_putchar_fd(e->board[0][16], 2);
+	//	ft_putchar_fd(']', 2);
+		while (e->board[y][x])
+		{
+			if (e->board[y][x] == ft_toupper(c))
+			{
+			fprintf(stderr, "il est la x:%d y:%d [%c]\n", x, y, e->board[y][x]);
+				break;
+			}
+			fprintf(stderr, "x:%d y:%d [%c]\n", x, y, e->board[y][x]);
+			x++;
+		}
+		y++;
+	}
+}
+
+void		fill_piece(t_env *e)
 {
 	int		i;
 	int		j;
+	int		r;
 
+	r = 0;
 	i = 0;
-	while (get_next_line(0, &(e)->buff) > 0 && i < y)
+	while (get_next_line(0, &(e)->buff) > 0 && i < e->spy)
 	{
 		j = 0;
-		while (j < x)
+		while (j < e->spx)
 		{
 			e->piece[i][j] = e->buff[j];
+			if (!r && e->piece[i][j] == '*')
+			{
+				e->px = j;
+				e->py = i;
+				r = 1;
+			}
 			j++;
 		}
 		i++;
@@ -85,43 +114,34 @@ void		fill_piece(t_env *e, int x, int y)
 void		add_piece(t_env *e)
 {
 	char	*addr;
-	int		x;
-	int		y;
 	int		i;
 
 	i = 0;
 	addr = ft_strchr(e->buff, ' ');
-	y = ft_atoi(addr++);
+	e->spy = ft_atoi(addr++);
 	addr = ft_strchr(addr, ' ');
-	x = ft_atoi(addr++);
-	e->piece = (char **)malloc(sizeof(char *) * (y + 1));
-	e->piece[y] = NULL;
-	while (i < y)
+	e->spx = ft_atoi(addr++);
+	e->piece = (char **)malloc(sizeof(char *) * (e->spy + 1));
+	e->piece[e->spy] = NULL;
+	while (i < e->spy)
 	{
-		e->piece[i] = (char *)malloc(sizeof(char) * (x + 1));
-		e->piece[i][x] = '\0';
+		e->piece[i] = (char *)malloc(sizeof(char) * (e->spx + 1));
+		e->piece[i][e->spx] = '\0';
 		i++;
 	}
-	fill_piece(e, x, y);
+	fill_piece(e);
 }
 
-void		play_mf(t_env *e, char c)
-{
-(void)e;
-(void)c;
-}
 
-#include <stdio.h>
-void		call_fctn(t_env *e, char c)
+
+void		call_fctn(t_env *e)
 {
 	if (e->buff[0] =='P' && e->buff[1] == 'l')
-		board_alloc(e);
+		board_alloc(e, -1);
 	else if (e->buff[0] == ' ' || e->buff[0] == '0')
 		fill_board(e);
 	else if (e->buff[0] == 'P' && e->buff[1] == 'i')
 		add_piece(e);
-	else
-		play_mf(e, c);
 }
 
 int			main()
@@ -132,7 +152,8 @@ int			main()
 	if (get_next_line(0, &(e).buff) > 0)
 		c = e.buff[10] == '1' ? 'o' : 'x';
 	while (get_next_line(0, &(e).buff) > 0)
-		call_fctn(&e, c);
+		call_fctn(&e);
+	play_mf(&e, c);
 
 
 
